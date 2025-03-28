bc2f7c050677f94ad810d3b85bd07c17de951ae1
saheb <sahebs450@gmail.com>
2025-03-28 16:28:26 +0530

    all things are done  till run at local

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="saheb sarkar",
+    author_email="sahebs450@gmail.com",
     packages=find_packages()
 )
